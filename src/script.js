316a2ad3551492650b7160d0367596657038b308
Jhom <jhomwan238@gmail.com>
2022-10-18 17:51:05 +0700

    refactor code to make it cleaner

--- src/script.js
+++ src/script.js
@@ -1,112 +1,108 @@
-const container = document.querySelector('.container')
+const container = document.querySelector(".container");
 
-//initialize 
+//initialize
 let boxesArray = [];
 let dateOfLastClick;
 
-if(localStorage.getItem("boxesArray") === null) {
-    for(let i = 0; i < 100; i++){
-        boxesArray.push(false);
-    }
-    localStorage.setItem("boxesArray", JSON.stringify(boxesArray));
-}else {
-    boxesArray = JSON.parse(localStorage.getItem("boxesArray"));
-    dateOfLastClick = localStorage.getItem("dateOfLastClick");    
+//get values from localStorage
+if (localStorage.getItem("boxesArray") === null) {
+  for (let i = 0; i < 100; i++) {
+    boxesArray.push(false);
+  }
+  localStorage.setItem("boxesArray", JSON.stringify(boxesArray));
+} else {
+  boxesArray = JSON.parse(localStorage.getItem("boxesArray"));
+  dateOfLastClick = localStorage.getItem("dateOfLastClick");
 }
 
 renderBoxes();
 
 function renderBoxes() {
+  boxesArray.forEach((isThisBoxFilled, boxIndex) => {
+    const box = document.createElement("div");
+    box.id = boxIndex;
+    box.classList.add("box");
 
-    boxesArray.forEach((isThisBoxFilled,boxIndex) => {
-        const box = document.createElement('div')
-        box.id = boxIndex
-        box.classList.add('box')
-
-        if(isThisBoxFilled){
-            updateBoxStatus(box)
-        }else{
-            makeBoxClickable(box, boxIndex)
-        }
-
-        container.appendChild(box)
-
-    })
-
-    if(!isTodayClickable()){
-        makeAllBoxesUnclickable();
+    if (isThisBoxFilled) {
+      fillUpBox(box);
+    } else {
+      makeBoxClickable(box, boxIndex);
     }
+
+    container.appendChild(box);
+  });
+
+  if (!isTodayClickable()) {
+    makeAllBoxesUnclickable();
+  }
 }
 
-function isTodayClickable(){
-    if(dateOfLastClick == getTodayDate())
-        return false
-    else
-        return true
+function fillUpBox(boxEl) {
+  boxEl.classList.add("clicked");
 }
 
-function getTodayDate(){
-    const date = new Date();
-    const today = date.getDate();
-    return today.toString();
+function makeBoxClickable(boxEl, boxIndex) {
+  boxEl.addEventListener("click", () => {
+    boxIsClicked(boxEl, boxIndex);
+  });
 }
 
-function makeBoxClickable(boxEl, boxIndex){
-   boxEl.addEventListener('click', () => {
-        boxIsClicked(boxEl, boxIndex)
-   })
+function makeAllBoxesUnclickable() {
+  boxesArray.forEach((isThisBoxFilled, boxIndex) => {
+    box = document.getElementById(boxIndex);
+    //Remove event listener of all elements
+    box.replaceWith(box.cloneNode(true));
+  });
+}
+
+//DATE
+function isTodayClickable() {
+  if (dateOfLastClick == getTodayDate()) return false;
+  else return true;
+}
+
+function getTodayDate() {
+  const date = new Date();
+  const today = date.getDate();
+  return today.toString();
 }
 
 //This function will be executed when a box is clicked
-function boxIsClicked(boxEl, boxIndex){
-    boxesArray[boxIndex] = true
-    dateOfLastClick = getTodayDate()
-    localStorage.setItem("boxesArray", JSON.stringify(boxesArray));
-    localStorage.setItem("dateOfLastClick", getTodayDate());
+function boxIsClicked(boxEl, boxIndex) {
+  boxesArray[boxIndex] = true;
+  dateOfLastClick = getTodayDate();
+  localStorage.setItem("boxesArray", JSON.stringify(boxesArray));
+  localStorage.setItem("dateOfLastClick", getTodayDate());
 
-    updateBoxStatus(boxEl)
-    makeAllBoxesUnclickable()
-    updateStatus()
-    updateCount()
-
+  fillUpBox(boxEl);
+  makeAllBoxesUnclickable();
+  updateClickStatus();
+  updateCount();
 }
 
-function updateBoxStatus(boxEl){
-    boxEl.classList.add('clicked')
+//TODAY CLICK STATUS
+const statusDiv = document.querySelector(".status");
+const completed = document.querySelector(".completed");
+const incomplete = document.querySelector(".incomplete");
+
+updateClickStatus();
+
+function updateClickStatus() {
+  if (isTodayClickable()) {
+    completed.style.display = "none";
+    incomplete.style.display = "block";
+  } else {
+    completed.style.display = "block";
+    incomplete.style.display = "none";
+  }
 }
 
-function makeAllBoxesUnclickable(){
-    boxesArray.forEach((isThisBoxFilled, boxIndex) => {
-        box = document.getElementById(boxIndex)
-        //Remove event listeners from Element
-        box.replaceWith(box.cloneNode(true));   
-    })
-}
+//COUNTER
+const countEl = document.getElementById("count");
 
-//TODAY STATUS
-const statusDiv = document.querySelector('.status')
-const completed = document.querySelector('.completed')
-const incomplete = document.querySelector('.incomplete')
+updateCount();
 
-updateStatus()
-
-function updateStatus() {
-    if(isTodayClickable()){
-        completed.style.display = 'none'
-        incomplete.style.display = 'block'
-    }else{
-        completed.style.display = 'block'
-        incomplete.style.display = 'none'
-    }
-}
-
-//UPDATE COUNT NUMBER
-
-const countEl = document.getElementById('count')
-
-updateCount()
-
-function updateCount(){
-    const boxesArray_true = boxesArray.filter(value => value === true)
-    countEl.innerText = boxesArray_true.length
+function updateCount() {
+  const boxesArray_true = boxesArray.filter((value) => value === true);
+  countEl.innerText = boxesArray_true.length;
 }
